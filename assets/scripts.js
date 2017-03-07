debf04700b0ae839346fbedb4616d494551784b0
Amy Hwang <amyhwang1218@gmail.com>
2017-03-07 11:07:21 -0800

    add sticky nav and superbar and a functionality to to-the-top button

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,6 +1,34 @@
+//showing superbar
+$(function(){
+  $('.show-superbar').click(function(event){
+      event.stopPropagation();
+       $(".superbar").slideToggle().css('display', 'flex');
+  });
+  $(".superbar").on("click", function (event) {
+      event.stopPropagation();
+  });
+});
+$(document).on("click", function () {
+    $(".superbar").slideUp();
+});
+
+//fixed header on scroll
+var num = 92;
+$(window).bind('scroll', function () {
+    if ($(window).scrollTop() > num) {
+      $('.header').addClass('fixed');
+    } else {
+      $('.header').removeClass('fixed');
+    };
+    if ($(window).scrollTop() > num) {
+      $('.superbar').addClass('sup-fixed');
+    } else {
+      $('.superbar').removeClass('sup-fixed');
+    }
+});
+
+//sildeshow
 $(function() {
-
-
   $(".index-swiper").swiper({
       pagination: '.swiper-pagination',
       nextButton: '.swiper-button-next',
@@ -15,5 +43,15 @@ $(function() {
       },
       speed: 1000,
       autoplay: 5000
+  });
+});
+
+
+//to the top
+$(function() {
+  $('.to-the-top').click(function() {
+    $('body,html').animate({
+      scrollTop : 0
+    }, 800);
   });
 });
