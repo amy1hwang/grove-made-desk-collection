9db9d451eda76ad9e32086c1d23b09a385657d34
Amy Hwang <amyhwang1218@gmail.com>
2017-05-17 23:59:57 -0700

    add bullet points snippet and add product prices and titles

--- assets/scripts.js
+++ assets/scripts.js
@@ -14,20 +14,6 @@ $(document).on("click", function () {
     $(".superbar").slideUp();
 });
 
-//cart cart-clicked
-// $(function() {
-//   $('.cart-container').click(function(event) {
-//     event.stopPropagation();
-//     $(".cart-clicked").css('height', 'auto');
-//   });
-//   $(".cart-clicked").on("click", function(event) {
-//     event.stopPropagation();
-//   });
-// })
-// $(document).on("click", function() {
-//   $(".cart-clicked").css('height', '0');
-// })
-
 //fixed header on scroll
 var num = 0;
 $(window).bind('scroll', function () {
@@ -69,4 +55,31 @@ $(function() {
   });
 });
 
-
+//slide from right to left on scroll
+// $(function() {
+//   var $animation_elements = $('.collection-group__list li');
+//   var $window = $(window);
+//   $window.on('scroll', check_if_in_view);
+//   $window.trigger('scroll');
+//
+//   function check_if_in_view() {
+//     var window_height = $window.height();
+//     var window_top_position = $window.scrollTop();
+//     var window_bottom_position = (window_top_position + window_height);
+//
+//     $.each($animation_elements, function() {
+//       var $element = $(this);
+//       var element_height = $element.outerHeight();
+//       var element_top_position = $element.offset().top;
+//       var element_bottom_position = (element_top_position + element_height);
+//
+//       //check to see if this current container is within viewport
+//       if ((element_bottom_position >= window_top_position) &&
+//           (element_top_position <= window_bottom_position)) {
+//         $element.addClass('in-view');
+//       } else {
+//         $element.removeClass('in-view');
+//       }
+//     });
+//   }
+// });
