0abc13f124d1bbe3c6e93e1bf41c111422f2ef2a
Amy Hwang <amyhwang1218@gmail.com>
2017-05-19 02:52:11 -0700

    style home page

--- assets/scripts.js
+++ assets/scripts.js
@@ -40,42 +40,12 @@ $(function() {
       autoplay: 5000
   });
 
-
   //to the top
   $('.to-the-top').click(function() {
     $('body,html').animate({
       scrollTop : 0
     }, 800);
   });
-
-//slide from right to left on scroll
-// $(function() {
-//   var $animation_elements = $('.collection-group__list li');
-//   var $window = $(window);
-//   $window.on('scroll', check_if_in_view);
-//   $window.trigger('scroll');
-//
-//   function check_if_in_view() {
-//     var window_height = $window.height();
-//     var window_top_position = $window.scrollTop();
-//     var window_bottom_position = (window_top_position + window_height);
-//
-//     $.each($animation_elements, function() {
-//       var $element = $(this);
-//       var element_height = $element.outerHeight();
-//       var element_top_position = $element.offset().top;
-//       var element_bottom_position = (element_top_position + element_height);
-//
-//       //check to see if this current container is within viewport
-//       if ((element_bottom_position >= window_top_position) &&
-//           (element_top_position <= window_bottom_position)) {
-//         $element.addClass('in-view');
-//       } else {
-//         $element.removeClass('in-view');
-//       }
-//     });
-//   }
-// });
 
 
   $("[data-product-swiper]").swiper({
